1dd098e91803bf22b39bf72b75d999a15f4a0489
Joseph Aveltsev <aveltsev@gmail.com>
2019-11-05 19:25:40 +0200

    rewrite some functions

---
+++ Trees/BinarySearchTree.h
@@ -0,0 +1,315 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+
+struct Node
+{
+	int val;
+	Node* left;
+	Node* right;
+	Node* parent;
+
+	Node(int _val)
+	{
+		val = _val;
+		left = nullptr;
+		right = nullptr;
+		parent = nullptr;
+	}
+};
+
+class BinarySearchTree
+{
+private:
+	Node* root;
+	size_t pathlength = 0;
+
+	Node* insert(Node* root, int val)
+	{
+		if (root == nullptr)
+			root = new Node(val);
+		else if (val < root->val)
+		{
+			Node* lchild = insert(root->left, val);
+			root->left = lchild;
+			lchild->parent = root;
+		}
+		else
+		{
+			Node* rchild = insert(root->right, val);
+			root->right = rchild;
+			rchild->parent = root;
+		}
+
+		return root;
+	}
+
+	// Time complexity is O(h).
+	Node* search(int key, Node* root)
+	{
+		if (root == nullptr ||  key == root->val)
+		{
+			return root;
+		}
+
+		if (key < root->val) 
+			search(key, root->left);
+		else 
+			search(key, root->right);
+	}
+
+	// Time complexity is O(h).
+	Node* iterative_search(int key, Node* root)
+	{
+		while (root != nullptr && key != root->val)
+		{
+			if (key < root->val)
+				root = root->left;
+			else
+				root = root->right;
+		}
+		return root;
+	}
+
+	// Time complexity is O(h).
+	Node* successor(Node* root)
+	{
+		if (root->right != nullptr)
+			return min(root->right);
+
+		Node* temp = root->parent;
+		while (temp != nullptr && root == temp->right)
+		{
+			root = temp;
+			temp = temp->parent;
+		}
+		return temp;
+	}
+
+	// Time complexity is O(h).
+	Node* predecessor(Node* root)
+	{
+		if (root->left != nullptr)
+			return min(root->left);
+
+		Node* temp = root->parent;
+		while (temp != nullptr && root == temp->left)
+		{
+			root = temp;
+			temp = temp->parent;
+		}
+		return temp;
+	}
+
+	// Time complexity is O(h).
+	Node* max(Node* root)
+	{
+		while (root->right != nullptr)
+			root = root->right;
+		return root;
+	}
+
+	// Time complexity is O(h).
+	Node* min(Node* root)
+	{
+		while (root->left != nullptr)
+			root = root->left;
+		return root;
+	}
+
+	// Preorder traversal (root -> left -> right).
+	void preorder(Node* root)
+	{
+		if (root == nullptr)
+			return;
+
+		std::cout << root->val << " ";
+		preorder(root->left);
+		preorder(root->right);
+	}
+
+	// Tree traversals.
+	//Inorder traversal (left -> root -> right).
+	void inorder(Node* root) 
+	{
+		if (root == nullptr)
+			return;
+
+		inorder(root->left);
+		std::cout << root->val << " ";
+		inorder(root->right);
+	}
+
+	// Postorder traversal (left -> right -> root).
+	void postorder(Node* root)
+	{
+		if (root == nullptr)
+			return;
+
+		postorder(root->left);
+		postorder(root->right);
+		std::cout << root->val << " ";
+	}
+
+	Node* remove(int val, Node* root) {
+		Node* temp;
+
+		if (root == nullptr)
+			return nullptr;
+		else if (val < root->val)
+			root->left = remove(val, root->left);
+		else if (val > root->val)
+			root->right = remove(val, root->right);
+		else if (root->left && root->right)
+		{
+			temp = min(root->right);
+			root->val = temp->val;
+			root->right = remove(root->val, root->right);
+		}
+		else
+		{
+			temp = root;
+			if (root->left == nullptr)
+				root = root->right;
+			else if (root->right == nullptr)
+				root = root->left;
+			delete temp;
+		}
+
+		return root;
+	}
+
+	// UTILITY FUNCTIONS.
+	void transplant(Node* root, Node* subtree1, Node* subtree2)
+	{
+		// When subtree1 is the root.
+		// Otherwise, u is either a left child
+		// or a right child of its parent.
+		if (subtree1->parent == nullptr)
+			root = subtree2;
+		else if (subtree1 == subtree1->parent->left)
+			subtree1->parent->left = subtree2;
+		else
+			subtree1->parent->right = subtree2;
+
+		if (subtree2 != nullptr)
+			subtree2->parent = subtree1->parent;
+	}
+
+	// Runs in O(h) time.
+	void remove(Node* root, Node* todelete)
+	{
+		Node* suc;
+
+		// If node todelete has no left child.
+		if (todelete->left == nullptr)
+			transplant(root, todelete, todelete->right);
+
+		// If node todelete has a left child but no right child.
+		else if (todelete->right == nullptr)
+			transplant(root, todelete, todelete->left);
+
+		// If node todelete has two children.
+		else
+		{
+			// Finds the successor of todelete.
+			suc = min(todelete->right);
+
+			// If suc is not todelete's right child then
+			// replace suc as a child of its parent by suc’s right child 
+			// and turn todelete’s right child into suc’s right child, 
+			// and then lines 10–12 replace todelete as a child of its
+			// parent by suc and replace suc’s left child by todelete’s left child.
+			if (suc->parent != todelete)
+			{
+				transplant(root, suc, suc->right);
+				suc->right = todelete->right;
+				suc->right->parent = suc;
+			}
+
+			// If successor is todelete's right child
+			// then replace todelete as a child of its parent by suc
+			// and replace suc's left child by todelete's left child.
+			transplant(root, todelete, suc);
+			suc->left = todelete->left;
+			suc->left->parent = suc;
+		}
+
+	}
+
+	void destroy(Node* root)
+	{
+		if (root == nullptr)
+			return;
+		else
+		{
+			destroy(root->left);
+			destroy(root->right);
+			delete root;
+		}
+		return;
+	}	
+
+public:
+	BinarySearchTree()
+	{
+		root = nullptr;
+	}
+
+	~BinarySearchTree()
+	{
+		destroy(root);
+		root = nullptr;
+	}
+
+	void insert(int val) 
+	{
+		root = insert(root, val);
+	}
+
+	void display() 
+	{
+		inorder(root);
+		std::cout << std::endl;
+	}
+
+	void search(int key)
+	{
+		search(key, root);
+	}
+
+	// Search using while loop. On most computers works faster.
+	void iterative_search(int key)
+	{
+		iterative_search(key, root);
+	}
+
+	size_t GetPathLength()
+	{
+		return pathlength;
+		pathlength = 0;
+	}
+	
+	int max()
+	{
+		return max(root)->val;
+	}
+
+	int min()
+	{
+		return min(root)->val;
+	}
+
+	void remove(int key)
+	{
+		remove(key, root);
+		std::cout << "Removed successfully!\n";
+	}
+
+	void removeV2(int key)
+	{
+		Node* todel = search(key, root);
+		remove(root, todel);
+	}
+};
+
--- Trees/Trees.cpp
+++ Trees/Trees.cpp
@@ -1,20 +1,22 @@
-// Trees.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "BinarySearchTree.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(nullptr));
+
+	BinarySearchTree bst;
+	bst.insert(18);
+	bst.insert(15);
+	bst.insert(1);
+	bst.insert(10);
+	bst.insert(30);
+	bst.insert(40);
+	bst.insert(50);
+	bst.insert(40);
+	bst.insert(100);
+
+	bst.display();
+	bst.removeV2(30);
+	bst.display();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
